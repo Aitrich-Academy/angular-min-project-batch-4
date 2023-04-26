1ec5a279729bdc1b3d1894c6fb9cacdf9d63615e
shirin vr <shirinvr1725@gmail.com>
2023-04-26 13:14:35 +0530

    completed search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { BooktableComponent } from './booktable/booktable.component';
 import { MenupageComponent } from './menupage/menupage.component';
 import { OurstoryComponent } from './ourstory/ourstory.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   { path:'', component:BooktableComponent},
   { path: 'home', component:BooktableComponent},
   { path:'menu', component:MenupageComponent},
-  { path: 'about', component:OurstoryComponent}
+  { path: 'about', component:OurstoryComponent},
+  { path: 'contact', component:ContactusComponent}
 ];
 
 @NgModule({
